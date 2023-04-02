a1f5ffff162cfcf081bda7d1094f6a464ab46727
ian <38461291+stutterrrrer@users.noreply.github.com>
2023-04-02 14:35:15 +0100

    project setup

---
+++ src/main/java/com/example/govalleycoursemanagement/GoValleyCourseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.govalleycoursemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoValleyCourseManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoValleyCourseManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/govalleycoursemanagement/GoValleyCourseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.govalleycoursemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoValleyCourseManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
